e3364d6587affef1bd310837fb195e3da7df1e22
amauri-castro <amauri.p.castro@gmail.com>
2024-06-06 22:03:48 -0300

    Implementado relacionamento bidirecional entre Book e Publisher

--- src/main/java/com/bookstore/jpa/models/Book.java
+++ src/main/java/com/bookstore/jpa/models/Book.java
@@ -17,6 +17,10 @@ public class Book implements Serializable {
     @Column(nullable = false, unique = true)
     private String title;
 
+    @ManyToOne
+    @JoinColumn(name = "publisher_id")
+    private Publisher publisher;
+
     public UUID getId() {
         return id;
     }
@@ -31,5 +35,13 @@ public class Book implements Serializable {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 }
